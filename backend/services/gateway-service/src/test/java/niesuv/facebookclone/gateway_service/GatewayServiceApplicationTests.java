95c182e4267a787d8ae0d3a9e840c266a2a8e9f5
pnngnas <huusangvtvip@gmail.com>
2024-11-02 12:44:13 +0700

    add gateway

--- backend/services/gateway-service/src/test/java/niesuv/facebookclone/gateway_service/GatewayServiceApplicationTests.java
+++ backend/services/gateway-service/src/test/java/niesuv/facebookclone/gateway_service/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package niesuv.facebookclone.eurekaserver;
+package niesuv.facebookclone.gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
